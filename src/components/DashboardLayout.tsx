a1362fd140f1dce513bc771e4c875241880d8b4a
[dyad] <bharatbihat@gmail.com>
2025-09-20 18:38:14 +0530

    [dyad] Added temporary borders to dashboard layout for debugging - wrote 1 file(s)

--- src/components/DashboardLayout.tsx
+++ src/components/DashboardLayout.tsx
@@ -8,11 +8,11 @@ interface DashboardLayoutProps {
 
 const DashboardLayout: React.FC<DashboardLayoutProps> = ({ children }) => {
   return (
-    <div className="flex min-h-screen bg-fintech-dashboard-bg">
+    <div className="flex min-h-screen bg-fintech-dashboard-bg border-2 border-red-500"> {/* Added red border */}
       <SidebarNav />
-      <div className="flex flex-col flex-1">
+      <div className="flex flex-col flex-1 border-2 border-blue-500"> {/* Added blue border */}
         <DashboardHeader />
-        <main className="flex-1 p-6 overflow-y-auto">
+        <main className="flex-1 p-6 overflow-y-auto border-2 border-green-500"> {/* Added green border */}
           {children}
         </main>
       </div>
