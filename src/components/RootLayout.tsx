f7226a04b007cda5e32a00501472042b87df3db0
[dyad] <bharatbihat@gmail.com>
2025-09-25 17:11:18 +0530

    [dyad] Removed flickering background video - wrote 1 file(s), deleted 1 file(s)

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -5,12 +5,12 @@ import { Outlet } from 'react-router-dom';
 import AiChatbot from './AiChatbot';
 import ApplyNowSidebarButton from './ApplyNowSidebarButton';
 import GlobalAnimatedBackground from './GlobalAnimatedBackground'; // Import the new component
-import VideoBackground from './VideoBackground'; // New import
+// import VideoBackground from './VideoBackground'; // New import - REMOVED
 
 const RootLayout = () => {
   return (
     <>
-      <VideoBackground /> {/* Add the video background here */}
+      {/* <VideoBackground /> Removed the video background */}
       <GlobalAnimatedBackground /> {/* Add the global animated background here */}
       <Outlet />
       <AiChatbot />
