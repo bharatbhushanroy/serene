227b1f821edaf85f7573e79a6e0a8ec2671aeeaf
[dyad] <bharatbihat@gmail.com>
2025-09-27 14:23:14 +0530

    [dyad] Fixed unclosed Link tag in TripleMobileMockup.tsx - wrote 1 file(s)

--- src/components/TripleMobileMockup.tsx
+++ src/components/TripleMobileMockup.tsx
@@ -141,7 +141,9 @@ const TripleMobileMockup: React.FC<TripleMobileMockupProps> = () => {
                 <p className="text-sm font-semibold">Credit score</p>
                 <p className="text-xs text-gray-400">Check now</p>
               </div>
-              <img src="/public/placeholder.svg" alt="Credit score" className="h-8 w-8" width={32} height={32} loading="lazy" /> {/* Added width/height and lazy loading */}
+              <Link to="/products/free-credit-score">
+                <img src="/public/placeholder.svg" alt="Credit score" className="h-8 w-8" width={32} height={32} loading="lazy" /> {/* Added width/height and lazy loading */}
+              </Link>
             </div>
           </div>
         </div>
